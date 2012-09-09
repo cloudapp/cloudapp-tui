a2a63e08095792ed58b73e688b4d41805d390754
Larry Marburger <larry@marburger.cc>
2012-09-08 21:16:33 -0400

    Show drop created date and views

--- lib/cloudapp/cli/drops_renderer.rb
+++ lib/cloudapp/cli/drops_renderer.rb
@@ -6,7 +6,30 @@ module CloudApp
       end
 
       def render
-        @drops.map(&:name).join("\n")
+        @drops.map {|drop|
+          [ drop.name,
+            "  #{ pretty_date(drop.created) }, #{ drop.views }"
+          ].join("\n")
+        }.join("\n")
+      end
+
+    private
+
+      def pretty_date(date)
+        diff = (Time.now - date.to_time).to_i
+        case diff
+          when 0               then 'just now'
+          when 1               then 'a second ago'
+          when 2..59           then diff.to_s+' seconds ago'
+          when 60..119         then 'a minute ago' #120 = 2 minutes
+          when 120..3540       then (diff/60).to_i.to_s+' minutes ago'
+          when 3541..7100      then 'an hour ago' # 3600 = 1 hour
+          when 7101..82800     then ((diff+99)/3600).to_i.to_s+' hours ago'
+          when 82801..172000   then 'a day ago' # 86400 = 1 day
+          when 172001..518400  then ((diff+800)/(60*60*24)).to_i.to_s+' days ago'
+          when 518400..1036800 then 'a week ago'
+          else ((diff+180000)/(60*60*24*7)).to_i.to_s+' weeks ago'
+        end
       end
     end
   end
--- spec/cloudapp/cli/drops_renderer_spec.rb
+++ spec/cloudapp/cli/drops_renderer_spec.rb
@@ -1,26 +1,42 @@
 require 'helper'
 require 'cloudapp/cli/drops_renderer'
+require 'date'
 
 describe CloudApp::CLI::DropsRenderer do
   describe '#render' do
-    let(:renderables) {[ RenderableDouble.new('one'),
-                         RenderableDouble.new('two'),
-                         RenderableDouble.new('three') ]}
+    let(:renderables) {[ RenderableDouble.new('one',   DateTime.now, 3),
+                         RenderableDouble.new('two',   DateTime.now - 1, 2),
+                         RenderableDouble.new('three', DateTime.now, 1) ]}
     subject { CloudApp::CLI::DropsRenderer.new(renderables).render }
 
-    it 'renders a list of names' do
-      expected = "one\ntwo\nthree"
+    it 'renders each drop' do
+      expected = <<-END.chomp
+one
+  just now, 3
+two
+  a day ago, 2
+three
+  just now, 1
+END
       subject.should eq(expected)
     end
   end
 end
 
-RenderableDouble = Struct.new :name
+RenderableDouble = Struct.new :name, :created, :views
 
 describe RenderableDouble do
   subject { RenderableDouble.new }
 
   it 'has a name' do
     subject.should respond_to(:name)
+  end
+
+  it 'has a created date' do
+    subject.should respond_to(:created)
+  end
+
+  it 'has views' do
+    subject.should respond_to(:views)
   end
 end
